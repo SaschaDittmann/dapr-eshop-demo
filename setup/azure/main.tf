5a049dc6bff01946478366bf5c872739b6a5ef8d
Sascha Dittmann <Sascha5812@gmx.de>
2021-05-04 01:04:46 +0200

    Add CosmosDB and K8s Deployment for webapp

--- setup/azure/main.tf
+++ setup/azure/main.tf
@@ -53,6 +53,12 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = false
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      az acr build -t ${self.login_server}/webapp -r ${self.name} --no-logs --no-wait ../.. -f ../../Dockerfile.webapp
+    EOT
+  }
 }
 
 resource "azurerm_role_assignment" "aks_sp_container_registry" {
@@ -124,6 +130,118 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     command = <<-EOT
       az aks get-credentials -g ${azurerm_resource_group.rg.name} -n ${azurerm_kubernetes_cluster.k8s.name} --overwrite-existing
       dapr init -k --enable-ha=true
+      kubectl apply -f components/.
     EOT
+  }
+}
+
+resource "azurerm_cosmosdb_account" "cosmosdb" {
+  name                = "${var.prefix}-cosmosdb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "statestore" {
+  name                = "statestore"
+  resource_group_name = azurerm_cosmosdb_account.cosmosdb.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdb.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "states" {
+  name                  = "states"
+  resource_group_name   = azurerm_cosmosdb_account.cosmosdb.resource_group_name
+  account_name          = azurerm_cosmosdb_account.cosmosdb.name
+  database_name         = azurerm_cosmosdb_sql_database.statestore.name
+  partition_key_path    = "/id"
+  partition_key_version = 1
+  throughput            = 400
+}
+
+resource "kubernetes_secret" "cosmosdb" {
+  metadata {
+    name = "azure-cosmosdb"
+  }
+  data = {
+    url       = "${azurerm_cosmosdb_account.cosmosdb.endpoint}"
+    masterKey = "${azurerm_cosmosdb_account.cosmosdb.primary_key}"
+  }
+  type = "Opaque"
+}
+
+resource "kubernetes_deployment" "webapp" {
+  metadata {
+    name = "webapp"
+    labels = {
+      app = "webapp"
+    }
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "webapp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "webapp"
+        }
+        annotations = {
+          "dapr.io/enabled"  = "true"
+          "dapr.io/app-id"   = "eshop"
+          "dapr.io/app-port" = "80"
+        }
+      }
+      spec {
+        container {
+          image             = "${azurerm_container_registry.acr.login_server}/webapp:latest"
+          name              = "aspnet"
+          image_pull_policy = "Always"
+          port {
+            container_port = 80
+          }
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 80
+            }
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "webapp" {
+  metadata {
+    name = "webapp"
+    labels = {
+      app = "webapp"
+    }
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.webapp.metadata.0.labels.app
+    }
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
   }
 }
