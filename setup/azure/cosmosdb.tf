30b9d60fa78c9767999d6d8c3bf716608fa411f6
Sascha Dittmann <Sascha5812@gmx.de>
2021-05-04 15:57:04 +0200

    feat: use new partition key

--- setup/azure/cosmosdb.tf
+++ setup/azure/cosmosdb.tf
@@ -26,7 +26,7 @@ resource "azurerm_cosmosdb_sql_container" "states" {
   resource_group_name   = azurerm_cosmosdb_account.cosmosdb.resource_group_name
   account_name          = azurerm_cosmosdb_account.cosmosdb.name
   database_name         = azurerm_cosmosdb_sql_database.statestore.name
-  partition_key_path    = "/id"
+  partition_key_path    = "/partitionKey"
   partition_key_version = 1
   throughput            = 400
 }
