1fd9101bf198cbb8a8c28817849b47c3967f5405
Sascha Dittmann <Sascha5812@gmx.de>
2021-05-20 18:41:27 +0200

    feat: Add Shopping Cart to Email

--- OrderService/Controllers/OrderController.cs
+++ OrderService/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Dapr;
 using Dapr.Client;
@@ -33,21 +35,47 @@ namespace OrderService.Controllers
             // TODO: Process Order
             _logger.LogInformation($"Received an order with {items.Count} items.");
 
-            _logger.LogDebug("Retrieving Secrets");
+            await SendEmail(items, daprClient);
+
+            return new OrderResponse();
+        }
+
+        private async Task SendEmail(List<Item> items, DaprClient daprClient)
+        {
+            _logger.LogDebug("Retrieving Email Default Setting");
             var defaultEmailSettings = await daprClient.GetSecretAsync(
                 "kubernetes", "email-default-settings");
+
+            _logger.LogDebug("Building Email Content");
+            var emailContent = new StringBuilder();
+            emailContent.Append("<h1>Order Confirmation</h1>");
+            emailContent.Append("e-Shop Dapr Demo - Order Confimation<br/>");
+
+            emailContent.Append("<table cellpadding=\"2\" cellspacing=\"2\" border=\"1\">");
+            emailContent.Append("<tr><th>Id</th><th>Name</th><th>Price</th><th>Quantity</th><th>Sub Total</th></tr>");
+            foreach(var item in items) {
+                emailContent.Append("<tr>");
+                emailContent.Append($"<td>{item.Product.Id}</td>");
+                emailContent.Append($"<td>{item.Product.Name}</td>");
+                emailContent.Append($"<td>{item.Product.Price}</td>");
+                emailContent.Append($"<td>{item.Quantity}</td>");
+                emailContent.Append($"<td>{item.Product.Price * item.Quantity}</td>");
+                emailContent.Append("</tr>");
+            }
+            emailContent.Append("<tr>");
+            emailContent.Append("<td align=\"right\" colspan=\"4\">Sum</td>");
+            emailContent.Append($"<td>{items.Sum(item => item.Product.Price * item.Quantity)}</td>");
+            emailContent.Append("</tr></table>");
 
             _logger.LogDebug("Sending Confirmation Email");
             await daprClient.InvokeBindingAsync<string>(
                 "email", 
                 "create", 
-                "<h1>Order Confirmation</h1>Thank you for your order at e-Shop Dapr Demo.",
+                emailContent.ToString(),
                 new Dictionary<string, string>{
                     {"emailTo", defaultEmailSettings["email-to"]},
-                    {"subject", "e-Shop Dapr Demo - Order Confimation"},
+                    {"subject", "Thank you for your order at e-Shop Dapr Demo"},
                 });
-
-            return new OrderResponse();
         }
     }
 }
